82cd794850cb2014091b7636312b88b710ab28c1
Quni <quni_jhuang@mail.chuangyo.net>
2025-12-09 16:14:50 +0800

    [更新]gotop&註冊頁邏輯

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <ThemeManager v-if="enableThemeManager">
+    <Layout />
+  </ThemeManager>
+  <Layout v-else />
+</template>
+
+<script setup>
+import ThemeManager from '@/theme/components/ThemeManager.vue'
+import Layout from '@/components/Layout.vue'
+
+// 控制 ThemeManager 開關, true: 開啟, false: 關閉
+const enableThemeManager = ref(true)
+provide('enableThemeManager', enableThemeManager)
+</script>
